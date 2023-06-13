10af7f03dbef2add18093c87a3d7285da7ee2e2a
17tia <17tia@403dev.com>
2023-06-13 23:54:25 +0900

    環境整備

--- src/deploy.js
+++ src/deploy.js
@@ -3,13 +3,14 @@ const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
-const CLIENT_ID = process.env.clientId;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`../commands/${file}`);
+	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 // Construct and prepare an instance of the REST module
@@ -19,11 +20,19 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 (async () => {
 	try {
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationCommands(CLIENT_ID),
-			{ body: commands },
-		);
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		if (GUILD_ID) {
+			const data = await rest.put(
+				Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+				{ body: commands },
+			);
+			console.log(`ギルドコマンドの登録に成功 ${data.length} application (/) commands.`);
+		} else {
+			const data = await rest.put(
+				Routes.applicationCommands(CLIENT_ID),
+				{ body: commands },
+			);
+			console.log(`グローバルコマンドの登録に成功 ${data.length} application (/) commands.`);
+		}
 	}
 	catch (error) {
 		// And of course, make sure you catch and log any errors!
