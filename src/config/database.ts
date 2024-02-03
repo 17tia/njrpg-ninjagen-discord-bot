4c99538b2a56520fa8874cdcc710ccbf697a27a1
17tia <17tia@403dev.com>
2024-02-03 21:07:48 +0900

    sequelizeを導入

---
+++ src/config/database.ts
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: process.env.DB_DIARECT,
+  storage: process.env.DB_STORAGE,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+}
